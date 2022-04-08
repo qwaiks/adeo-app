bd96b4e37375a505e458811c83bca1138a9ee8c7
qwaiks <kwaku315@gmail.com>
2022-04-08 22:03:18 +0000

    adeo  initial setup

---
+++ lib/constants/app_string.dart
@@ -0,0 +1,13 @@
+class AppString {
+  static const String appName = "Adeo test";
+  static const String appWelcome = "Welcome to the Adeo Experience";
+  static const String subscriptionPrompt =
+      "You currently have  NO Subscriptions.  First take a DIAGNOSTIC TEST  to determine the right course  for you. ";
+  static const String next = 'Next';
+  static const String skip = 'Skip';
+  static const String selectLevel = "Select your level";
+  static const String selectClass ="Select your class";
+  static const String score = 'Score';
+  static const String rank = 'Rank';
+  static const String timeTaken = 'Time taken';
+}
